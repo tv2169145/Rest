107d4dd3f0e4fb535a3267c5f75071dcf4692195
zhean <tv2169145@gmail.com>
2018-08-22 22:51:25 +0800

    model建立完成

--- app/Buyer.php
+++ app/Buyer.php
@@ -2,9 +2,12 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
 
-class Buyer extends Model
+class Buyer extends User
 {
     //
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
--- app/Seller.php
+++ app/Seller.php
@@ -2,9 +2,11 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
-
-class Seller extends Model
+class Seller extends User
 {
     //
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
