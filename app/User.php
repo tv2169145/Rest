68b34a8aa096143011b6da4f648b1bac699ecd41
zhean <tv2169145@gmail.com>
2018-08-24 04:51:04 +0800

    complete seed

--- app/User.php
+++ app/User.php
@@ -15,6 +15,8 @@ class User extends Authenticatable
     const ADMIN_USER = 'true';
     const REGULAR_USER = 'false';
 
+    protected $table = 'users';
+
 
     /**
      * The attributes that are mass assignable.
@@ -52,7 +54,7 @@ class User extends Authenticatable
         return $this->admin == User::ADMIN_USER;
     }
 
-    public function generateVerificationCode()
+    public static function generateVerificationCode()
     {
         return str_random(40);
     }
